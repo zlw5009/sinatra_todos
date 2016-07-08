d32dc3184ccb3fe920926934325622cd188345dc
Zachary Williams <zlw5009@gmail.com>
2016-07-08 18:58:32 -0400

    Added validations for list creation

--- todo.rb
+++ todo.rb
@@ -15,16 +15,37 @@ get "/" do
   redirect "/lists"
 end
 
+# View all the lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error msg if the name is invalid; returns nil if name is valid
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters." 
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
+end
+
+# Create a new list
 post "/lists" do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  redirect "/lists"
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The '#{list_name}' list has been created successfully!"
+    redirect "/lists" 
+  end
 end
